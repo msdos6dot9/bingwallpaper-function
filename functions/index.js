eee96c211db8db6f12aebf858adfa7daebe3882a
WiNDOWS964ourU <78335324+WiNDOWS964ourU@users.noreply.github.com>
2026-07-05 19:11:24 +0800

    CFPages Test-2

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 import { handleRequest } from '../edge-functions/index.js';
 
 // for Cloudflare Pages Functions
-export default async function onRequest(context) {
-  return await handleRequest(context.request);
+export function onRequest(context) {
+  return new Response("Hello, world!");
 }
